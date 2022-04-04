1e64d59a7eaa2d13d0f1860e8c58a674dce6dafe
René Sackers <rene.sackers@live.com>
2022-04-04 12:32:18 +0200

    Initial commit

---
+++ src/RedditPlaceDiff/ApiModel.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+
+namespace RedditPlaceDiff;
+
+public class MapHistory
+{
+	[JsonProperty("file")]
+	public string File { get; set; }
+
+	[JsonProperty("reason")]
+	public string Reason { get; set; }
+
+	[JsonProperty("date")]
+	public object Date { get; set; }
+}
+
+public class Brands
+{
+	[JsonProperty("nodeheadlessV7")]
+	public int NodeheadlessV7 { get; set; }
+
+	[JsonProperty("userscriptV20")]
+	public int UserscriptV20 { get; set; }
+
+	[JsonProperty("nodeheadlessV6")]
+	public int NodeheadlessV6 { get; set; }
+
+	[JsonProperty("userscriptV23")]
+	public int UserscriptV23 { get; set; }
+
+	[JsonProperty("nodeheadlessV5")]
+	public int NodeheadlessV5 { get; set; }
+
+	[JsonProperty("nodeheadlessV4")]
+	public int NodeheadlessV4 { get; set; }
+
+	[JsonProperty("PlaceNLpythonV2")]
+	public int PlaceNLpythonV2 { get; set; }
+
+	[JsonProperty("nodeheadlessV3")]
+	public int NodeheadlessV3 { get; set; }
+
+	[JsonProperty("unknown")]
+	public int Unknown { get; set; }
+
+	[JsonProperty("userscriptV19")]
+	public int UserscriptV19 { get; set; }
+
+	[JsonProperty("userscriptV17")]
+	public int UserscriptV17 { get; set; }
+
+	[JsonProperty("userscriptV18")]
+	public int UserscriptV18 { get; set; }
+
+	[JsonProperty("userscriptV16")]
+	public int UserscriptV16 { get; set; }
+}
+
+public class ApiModel
+{
+	[JsonProperty("rawConnectionCount")]
+	public int RawConnectionCount { get; set; }
+
+	[JsonProperty("connectionCount")]
+	public int ConnectionCount { get; set; }
+
+	[JsonProperty("currentMap")]
+	public string CurrentMap { get; set; }
+
+	[JsonProperty("mapHistory")]
+	public List<MapHistory> MapHistory { get; set; }
+
+	[JsonProperty("brands")]
+	public Brands Brands { get; set; }
+
+	[JsonProperty("date")]
+	public long Date { get; set; }
+}
---
+++ src/RedditPlaceDiff/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using RedditPlaceDiff;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+
+var dataDir = AppContext.BaseDirectory;
+
+const int sectionSize = 1000;
+const int scaleFactor = 3;
+
+Console.WriteLine("Downloading command image");
+
+using var httpClient = new HttpClient();
+var json = await httpClient.GetStringAsync("https://placenl.noahvdaa.me/api/stats");
+var apiData = JsonConvert.DeserializeObject<ApiModel>(json);
+var mapUrl = "https://placenl.noahvdaa.me/maps/" + apiData.CurrentMap;
+var mapRequest = await httpClient.GetAsync(mapUrl);
+var imageStream = await mapRequest.Content.ReadAsStreamAsync();
+using var command = Image.Load<Rgba32>(imageStream, new PngDecoder());
+
+Console.WriteLine("Generating image");
+
+using var image = new Image<Rgba32>(sectionSize * 2 * scaleFactor, sectionSize * 2 * scaleFactor);
+
+image.ProcessPixelRows(accessor =>
+{
+	for (var y = 1; y < image.Height; y += scaleFactor)
+	{
+		var pixelRow = accessor.GetRowSpan(y);
+		
+		for (var x = 1; x < image.Width; x += scaleFactor)
+		{
+			var commandPixel = command[x / scaleFactor, y / scaleFactor];
+			if (commandPixel.A == 0)
+				continue;
+			
+			ref var pixel = ref pixelRow[x];
+
+			pixel = commandPixel;
+		}
+	}
+});
+
+var targetDir = Path.Combine(dataDir, "generated.png");
+image.Save(targetDir);
+Console.WriteLine($"Image saved to: {targetDir}");
